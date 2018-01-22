cb88dbda62bc22b8a0f2a3bed2b4fe94c3b23d27
MFISGOOD <turbot2@hotmail.com>
2018-01-22 15:06:50 +0100

    modifying report function

--- tests.js
+++ tests.js
@@ -1,10 +1,10 @@
 (function(){
 let numbers=[1,2,3];
-report(query().select().from(numbers).execute())
-report(query().select().execute())
-report(query().from(numbers).execute())
-report(query().execute())
-report(query().from(numbers).select().execute())
+report(query().select().from(numbers).execute(),JSON.stringify([1,2,3]))
+report(query().select().execute(),'[]')
+report(query().from(numbers).execute(),'[1,2,3]')
+report(query().execute(),'[]')
+report(query().from(numbers).select().execute(),'[1,2,3]')
 
 let persons=[
 {name:'Peter',profession:'teacher',age:20,maritalStatus:'married'},
@@ -23,9 +23,10 @@ return person.profession;
 }
 
 //SELECTprofessionFROMpersons
-report(query().select(profession).from(persons).execute())
+report(query().select(profession).from(persons).execute(),
+	'["teacher","teacher","teacher","scientific","scientific","scientific","politician"]')
 //["teacher","teacher","teacher","scientific","scientific","scientific","politician"]);
-report(query().select(profession).execute())
+report(query().select(profession).execute(),'[]')
 ////[]
 
 
@@ -34,12 +35,12 @@ return person.profession==='teacher';
 }
 
 //SELECTprofessionFROMpersonsWHEREprofession="teacher"
-report(query().select(profession).from(persons).where(isTeacher).execute())
+report(query().select(profession).from(persons).where(isTeacher).execute(),'["teacher","teacher","teacher"]')
 //["teacher","teacher","teacher"]);
 
 
 //SELECT*FROMpersonsWHEREprofession="teacher"
-report(query().from(persons).where(isTeacher).execute())
+report(query().from(persons).where(isTeacher).execute(),JSON.stringify(persons.slice(0,3)))
 //persons.slice(0,3));
 
 function name(person){
@@ -47,14 +48,10 @@ return person.name;
 }
 
 //SELECTnameFROMpersonsWHEREprofession="teacher"
-report(query().select(name).from(persons).where(isTeacher).execute())
+report(query().select(name).from(persons).where(isTeacher).execute(),'["Peter","Michael","Peter"]')
 //["Peter","Michael","Peter"]);
-report(query().where(isTeacher).from(persons).select(name).execute())
+report(query().where(isTeacher).from(persons).select(name).execute(),'["Peter","Michael","Peter"]')
 //,["Peter","Michael","Peter"]);
-
-
-
-
 
 
 persons=[
@@ -72,7 +69,8 @@ return person.profession;
 }
 
 //SELECT*FROMpersonsGROUPBYprofession<-BadinSQLbutpossibleinJavaScript
-report(query().select().from(persons).groupBy(profession).execute())
+report(query().select().from(persons).groupBy(profession).execute(),
+	'[["teacher",[{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"},{"name":"Michael","profession":"teacher","age":50,"maritalStatus":"single"},{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"}]],["scientific",[{"name":"Anna","profession":"scientific","age":20,"maritalStatus":"married"},{"name":"Rose","profession":"scientific","age":50,"maritalStatus":"married"},{"name":"Anna","profession":"scientific","age":20,"maritalStatus":"single"}]],["politician",[{"name":"Anna","profession":"politician","age":50,"maritalStatus":"married"}]]]')
 //,[["teacher",[{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"},{"name":"Michael","profession":"teacher","age":50,"maritalStatus":"single"},{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"}]],["scientific",[{"name":"Anna","profession":"scientific","age":20,"maritalStatus":"married"},{"name":"Rose","profession":"scientific","age":50,"maritalStatus":"married"},{"name":"Anna","profession":"scientific","age":20,"maritalStatus":"single"}]],["politician",[{"name":"Anna","profession":"politician","age":50,"maritalStatus":"married"}]]]);
 
 function isTeacher(person){
@@ -80,7 +78,8 @@ return person.profession==='teacher';
 }
 
 //SELECT*FROMpersonsWHEREprofession='teacher'GROUPBYprofession
-report(query().select().from(persons).where(isTeacher).groupBy(profession).execute())
+report(query().select().from(persons).where(isTeacher).groupBy(profession).execute(),
+	'[["teacher",[{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"},{"name":"Michael","profession":"teacher","age":50,"maritalStatus":"single"},{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"}]]]')
 //,[["teacher",[{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"},{"name":"Michael","profession":"teacher","age":50,"maritalStatus":"single"},{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"}]]]);
 
 function professionGroup(group){
@@ -88,7 +87,7 @@ return group[0];
 }
 
 //SELECTprofessionFROMpersonsGROUPBYprofession
-report(query().select(professionGroup).from(persons).groupBy(profession).execute())
+report(query().select(professionGroup).from(persons).groupBy(profession).execute(),'["teacher","scientific","politician"]')
 //,["teacher","scientific","politician"]);
 
 function name(person){
@@ -96,7 +95,15 @@ return person.name;
 }
 
 //SELECT*FROMpersonsWHEREprofession='teacher'GROUPBYprofession,name
-report(query().select().from(persons).groupBy(profession,name).execute())
+let result1 = [["teacher",[["Peter",[{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"},
+                     {"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"}]],["Michael",
+                     [{"name":"Michael","profession":"teacher","age":50,"maritalStatus":"single"}]]]],
+                ["scientific",[["Anna",[{"name":"Anna","profession":"scientific","age":20,"maritalStatus":"married"},
+                      {"name":"Anna","profession":"scientific","age":20,"maritalStatus":"single"}]],["Rose",
+                      [{"name":"Rose","profession":"scientific","age":50,"maritalStatus":"married"}]]]],
+                ["politician",[["Anna",[{"name":"Anna","profession":"politician","age":50,"maritalStatus":"married"}]]]
+                ]];
+report(query().select().from(persons).groupBy(profession,name).execute(),JSON.stringify(result1));
 //,[["teacher",[["Peter",[{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"},{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"}]],["Michael",[{"name":"Michael","profession":"teacher","age":50,"maritalStatus":"single"}]]]],["scientific",[["Anna",[{"name":"Anna","profession":"scientific","age":20,"maritalStatus":"married"},{"name":"Anna","profession":"scientific","age":20,"maritalStatus":"single"}]],["Rose",[{"name":"Rose","profession":"scientific","age":50,"maritalStatus":"married"}]]]],["politician",[["Anna",[{"name":"Anna","profession":"politician","age":50,"maritalStatus":"married"}]]]]]
 
 
@@ -108,8 +115,16 @@ function maritalStatus(person){
 return person.maritalStatus;
 }
 
+let result2=[["teacher",[["Peter",[[20,[["married",[{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"},
+{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"}]]]]]],["Michael",
+[[50,[["single",[{"name":"Michael","profession":"teacher","age":50,"maritalStatus":"single"}]]]]]]]],
+["scientific",[["Anna",[[20,[["married",[{"name":"Anna","profession":"scientific","age":20,"maritalStatus":"married"}]],
+["single",[{"name":"Anna","profession":"scientific","age":20,"maritalStatus":"single"}]]]]]],
+["Rose",[[50,[["married",[{"name":"Rose","profession":"scientific","age":50,"maritalStatus":"married"}]]]]]]]],
+["politician",[["Anna",[[50,[["married",[{"name":"Anna","profession":"politician","age":50,"maritalStatus":"married"}]]]]]]]]];
+
 //SELECT*FROMpersonsWHEREprofession='teacher'GROUPBYprofession,name,age
-report(query().select().from(persons).groupBy(profession,name,age,maritalStatus).execute())
+report(query().select().from(persons).groupBy(profession,name,age,maritalStatus).execute(),JSON.stringify(result2))
 //,[["teacher",[["Peter",[[20,[["married",[{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"},{"name":"Peter","profession":"teacher","age":20,"maritalStatus":"married"}]]]]]],["Michael",[[50,[["single",[{"name":"Michael","profession":"teacher","age":50,"maritalStatus":"single"}]]]]]]]],["scientific",[["Anna",[[20,[["married",[{"name":"Anna","profession":"scientific","age":20,"maritalStatus":"married"}]],["single",[{"name":"Anna","profession":"scientific","age":20,"maritalStatus":"single"}]]]]]],["Rose",[[50,[["married",[{"name":"Rose","profession":"scientific","age":50,"maritalStatus":"married"}]]]]]]]],["politician",[["Anna",[[50,[["married",[{"name":"Anna","profession":"politician","age":50,"maritalStatus":"married"}]]]]]]]]]);
 
 function professionCount(group){
@@ -117,7 +132,7 @@ return[group[0],group[1].length];
 }
 
 //SELECTprofession,count(profession)FROMpersonsGROUPBYprofession
-report(query().select(professionCount).from(persons).groupBy(profession).execute())
+report(query().select(professionCount).from(persons).groupBy(profession).execute(),'[["teacher",3],["scientific",3],["politician",1]]')
 //,[["teacher",3],["scientific",3],["politician",1]]);
 
 function naturalCompare(value1,value2){
@@ -131,7 +146,7 @@ return 0;
 }
 
 //SELECTprofession,count(profession)FROMpersonsGROUPBYprofessionORDERBYprofession
-report(query().select(professionCount).from(persons).groupBy(profession).orderBy(naturalCompare).execute())
+report(query().select(professionCount).from(persons).groupBy(profession).orderBy(naturalCompare).execute(),'[["politician",1],["scientific",3],["teacher",3]]')
 //,[["politician",1],["scientific",3],["teacher",3]]);
 
 
@@ -160,14 +175,14 @@ return isPrime(number)?'prime':'divisible';
 numbers=[1,2,3,4,5,6,7,8,9];
 
 //SELECT*FROMnumbers
-report(query().select().from(numbers).execute())
+report(query().select().from(numbers).execute(),'[1,2,3,4,5,6,7,8,9]')
 
 //SELECT*FROMnumbersGROUPBYparity
-report(query().select().from(numbers).groupBy(parity).execute())
+report(query().select().from(numbers).groupBy(parity).execute(),'[["odd",[1,3,5,7,9]],["even",[2,4,6,8]]]')
 //,[["odd",[1,3,5,7,9]],["even",[2,4,6,8]]]);
 
 //SELECT*FROMnumbersGROUPBYparity,isPrime
-report(query().select().from(numbers).groupBy(parity,prime).execute())
+report(query().select().from(numbers).groupBy(parity,prime).execute(),'[["odd",[["divisible",[1,9]],["prime",[3,5,7]]]],["even",[["prime",[2]],["divisible",[4,6,8]]]]]')
 //,[["odd",[["divisible",[1,9]],["prime",[3,5,7]]]],["even",[["prime",[2]],["divisible",[4,6,8]]]]]);
 
 function odd(group){
@@ -175,7 +190,7 @@ return group[0]==='odd';
 }
 
 //SELECT*FROMnumbersGROUPBYparityHAVING
-report(query().select().from(numbers).groupBy(parity).having(odd).execute())
+report(query().select().from(numbers).groupBy(parity).having(odd).execute(),'[["odd",[1,3,5,7,9]]]')
 //,[["odd",[1,3,5,7,9]]]);
 
 function descendentCompare(number1,number2){
@@ -183,7 +198,7 @@ return number2-number1;
 }
 
 //SELECT*FROMnumbersORDERBYvalueDESC
-report(query().select().from(numbers).orderBy(descendentCompare).execute())
+report(query().select().from(numbers).orderBy(descendentCompare).execute(),'[9,8,7,6,5,4,3,2,1]')
 //,[9,8,7,6,5,4,3,2,1]);
 
 function lessThan3(number){
@@ -195,7 +210,7 @@ return number>4;
 }
 
 //SELECT*FROMnumberWHEREnumber<3ORnumber>4
-report(query().select().from(numbers).where(lessThan3,greaterThan4).execute())
+report(query().select().from(numbers).where(lessThan3,greaterThan4).execute(),'[1,2,5,6,7,8,9]')
 //,[1,2,5,6,7,8,9]);
 
 
@@ -226,7 +241,7 @@ return 0;
 }
 }
 //SELECTname,sum(value)FROMpersonsORDERBYnaturalCompareGROUPBYnameGrouping
-report(query().select(sumValues).from(persons).orderBy(naturalCompare).groupBy(nameGrouping).execute())
+report(query().select(sumValues).from(persons).orderBy(naturalCompare).groupBy(nameGrouping).execute(),'[["Anna",4],["Michael",10],["Peter",10]]')
 //,[["Anna",4],["Michael",10],["Peter",10]]);
 
 numbers=[1,2,1,3,5,6,1,2,5,6];
@@ -240,7 +255,8 @@ return{value:group[0],frequency:group[1].length};
 }
 
 //SELECTnumber,count(number)FROMnumbersGROUPBYnumber
-report(query().select(frequency).from(numbers).groupBy(id).execute())
+report(query().select(frequency).from(numbers).groupBy(id).execute(),
+	'[{"value":1,"frequency":3},{"value":2,"frequency":2},{"value":3,"frequency":1},{"value":5,"frequency":2},{"value":6,"frequency":2}]')
 //,[{"value":1,"frequency":3},{"value":2,"frequency":2},{"value":3,"frequency":1},{"value":5,"frequency":2},{"value":6,"frequency":2}]);
 
 function greatThan1(group){
@@ -252,7 +268,8 @@ return group[0]%2===0;
 }
 
 //SELECTnumber,count(number)FROMnumbersGROUPBYnumberHAVINGcount(number)>1ANDisPair(number)
-report(query().select(frequency).from(numbers).groupBy(id).having(greatThan1).having(isPair).execute())
+report(query().select(frequency).from(numbers).groupBy(id).having(greatThan1).having(isPair).execute(),
+	'[{"value":2,"frequency":2},{"value":6,"frequency":2}]')
 //,[{"value":2,"frequency":2},{"value":6,"frequency":2}]);
 
 var teachers=[
@@ -287,14 +304,15 @@ return{studentName:join[1].studentName,teacherName:join[0].teacherName};
 }
 
 //SELECTstudentName,teacherNameFROMteachers,studentsWHEREteachers.teacherId=students.tutor
-report(query().select(student).from(teachers,students).where(teacherJoin).execute())
+report(query().select(student).from(teachers,students).where(teacherJoin).execute(),
+	'[{"studentName":"Michael","teacherName":"Peter"},{"studentName":"Rose","teacherName":"Anna"}]')
 //,[{"studentName":"Michael","teacherName":"Peter"},{"studentName":"Rose","teacherName":"Anna"}]);
 
 
 var numbers1=[1,2];
 var numbers2=[4,5];
 
-report(query().select().from(numbers1,numbers2).execute())
+report(query().select().from(numbers1,numbers2).execute(),'[[1,4],[1,5],[2,4],[2,5]]')
 //,[[1,4],[1,5],[2,4],[2,5]]);
 
 function tutor1(join){
@@ -302,7 +320,7 @@ return join[1].tutor==="1";
 }
 
 //SELECTstudentName,teacherNameFROMteachers,studentsWHEREteachers.teacherId=students.tutorANDtutor=1
-report(query().select(student).from(teachers,students).where(teacherJoin).where(tutor1).execute())
+report(query().select(student).from(teachers,students).where(teacherJoin).where(tutor1).execute(),'[{"studentName":"Michael","teacherName":"Peter"}]')
 //,[{"studentName":"Michael","teacherName":"Peter"}]);
 
 })()
